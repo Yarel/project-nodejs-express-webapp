f67431387f49be37752823edb0d4e82aaab9be3c
Sergio Guillen Mantilla <sergio@dreamcode.io>
2020-02-11 08:31:13 -0400

    Simulate data store

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,48 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var UserStore = require('../store/user');
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.send('respond with a resource');
+    res.send(UserStore.getUsers());
+});
+
+router.get('/:userId', (req, res) => {
+    const user = UserStore.getUserById(req.params.userId);
+    if (!user) {
+        return res.status(404).send({
+            message: 'User not found'
+        });
+    }
+    return res.send(user);
+});
+
+router.post('/', (req, res) => {
+    const data = req.body;
+    console.log('Creating user');
+    console.log(data);
+    UserStore.add(data);
+    return res.status(200).send({
+        'message': 'User created'
+    });
+});
+
+router.put('/:userId', (req, res) => {
+    const data = req.body;
+    data['id'] = req.params.userId;
+
+    UserStore.update(data);
+    return res.status(200).send({
+        message: 'updated'
+    });
+});
+
+router.delete('/:userId', (req, res) => {
+    const deleted = UserStore.delete(req.params.userId);
+    return res.status(deleted ? 200 : 404).send({
+        mesage: deleted ? 'deleted' : 'Not found'
+    });
 });
 
 module.exports = router;
---
+++ store/user.js
@@ -0,0 +1,47 @@
+class UserStore {
+
+  constructor() {
+    console.log('User Store constructuor');
+    this.users = {};
+  }
+
+  add(user) {
+    this.users[user.id] = user;
+    console.log('added');
+    console.log(this.users);
+    return true;
+  }
+
+  update(user) {
+    this.add(user);
+  }
+
+  delete(userId) {
+    if (this.users[userId]) {
+      delete this.users[userId];
+      return true;
+    }
+    return false;
+  }
+
+  getUsers() {
+    const result = [];
+    console.log('users:');
+    console.log(this.users);
+    Object.keys(this.users).forEach(key => {
+      console.log(`Adding ${key}`);
+      result.push(this.users[key]);
+    });
+    return result;
+  }
+
+  getUserById(userId) {
+    this.users[userId];
+  }
+}
+
+
+const userStore = new UserStore();
+
+
+module.exports = userStore;
